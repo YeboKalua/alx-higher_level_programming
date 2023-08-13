e2c95a6406bad22ceb836c124ea8cbe33f229bfa
Yebo Kalua <kaluayebo2@gmail.com>
2023-08-13 14:58:34 -0700

    exec

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    copy_list = my_list[:]
     my_list.reverse()
-    for number in my_list:
+    for number in copy_list:
         print("{:d}".format(number))
