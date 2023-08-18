e0b279b32c94c894dd90dbe7f6f664214f5750a9
Yebo Kalua <kaluayebo2@gmail.com>
2023-08-18 11:04:48 -0700

    best scores 1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_k = max(a_dictionary, key=a_dictionary.get)
-    return max_k
+    if a_dictionary is not None and len(a_dictionary) > 0:
+        max_k = max(a_dictionary, key=a_dictionary.get)
+        return max_k
+    else:
+        return None
