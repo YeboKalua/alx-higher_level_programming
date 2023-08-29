0bd559e8ad39dc1e7e6b1bd9cedc87ecd66d1fe9
Yebo Kalua <kaluayebo2@gmail.com>
2023-08-29 22:07:07 +0000

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    our_list = []
+    try:
+        new_list = my_list[:x]
+        for item in new_list:
+            our_list.append(item)
+            count += 1
+        our_list.append("\n")
+        for element in our_list:
+            print(element, end="")
+        return count
+    except IndexError:
+        x = count     
