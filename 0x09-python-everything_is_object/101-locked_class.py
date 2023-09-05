d8a2270c663f6968e66f38a50073bd028b0e6a1f
Yebo Kalua <kaluayebo2@gmail.com>
2023-09-05 14:19:34 -0700

    small changes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 class LockedClass:
+    """
+    class that sets name
+    """
+
     def __setattr__(self, name, value):
         if name == 'first_name':
             self.__dict__[name] = value
