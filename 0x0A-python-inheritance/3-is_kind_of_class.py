a77811fd7eee84c30168732781c9bd93801ae814
Yebo Kalua <kaluayebo2@gmail.com>
2023-09-11 16:07:09 -0700

    instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Checks if object is instance of a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function that checks instances
+    """
+    return isinstance(obj, a_class)
