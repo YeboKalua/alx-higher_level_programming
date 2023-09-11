a352085c003735c42a92bb33273e91f812958528
Yebo Kalua <kaluayebo2@gmail.com>
2023-09-11 15:59:22 -0700

    isinstance task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Checks if object belongs to a class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns true if object is an instance of a class
+    """
+    return isinstance(obj, a_class)
