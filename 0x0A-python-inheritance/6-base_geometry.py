c061c58c811658f4d242a50f8fbef8f43617cd2e
Yebo Kalua <kaluayebo2@gmail.com>
2023-09-11 16:28:44 -0700

    geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''adds functionality to the geometry class'''
+
+
+
+class BaseGeometry:
+    '''Geometry class'''
+    def area(self):
+        '''calculates area'''
+        raise Exception("area() is not implemented")
