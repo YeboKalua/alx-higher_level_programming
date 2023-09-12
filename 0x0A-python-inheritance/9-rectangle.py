4b74920d28df6307fbe8777bf13d1c60ac812c05
Yebo Kalua <kaluayebo2@gmail.com>
2023-09-11 17:30:14 -0700

    last retangke

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Rectangle class that inherits from geometry'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''inheritance from geometry'''
+    def __init__(self, width, height):
+        '''initializing variables'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+       '''calculates area'''
+       return self.__width * self.__height
+
+   def __str__(self):
+       '''string representation'''
+       return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
