2582d84428ddd08a63ea07b848f61c00b4c71d1a
Yebo Kalua <kaluayebo2@gmail.com>
2023-09-13 21:13:18 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""reads text file and prints to std output"""
 
+
+def read_file(filename=""):
+    """reads and print text file"""
+    with open(filename) as a_file:
+        print(a_file)
+        
