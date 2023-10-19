1f444db2f1dc4c1fdc66a598077d7abfcfd3f481
Yebo Kalua <kaluayebo2@gmail.com>
2023-10-19 14:32:20 -0700

    correction

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- reads from an imported table in database
-USE hbtn_0c_0;
-SOURCE /~/alx-higher_level_programming/0x0D-SQL_introduction/temperatures.sql;
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+-- USE hbtn_0c_0;
+-- SOURCE /~/alx-higher_level_programming/0x0D-SQL_introduction/temperatures.sql;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
