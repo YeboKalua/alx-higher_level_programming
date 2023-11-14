b7ea1b158aea4c54a55f4bd9a352947766b865d9
Yebo Kalua <kaluayebo2@gmail.com>
2023-11-14 10:32:41 -0800

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+function sort (arr) {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+const array = process.argv.slice(2);
+const newArray = sort(array);
+const num = newArray.length;
+if (num < 2) {
+  console.log(0);
+} else {
+  console.log(newArray[num - 2]);
+}
