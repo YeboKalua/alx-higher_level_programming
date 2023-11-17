4b901e052580606cf7c8db3a2d095635a3a925af
Yebo Kalua <kaluayebo2@gmail.com>
2023-11-17 11:08:03 -0800

    print

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      const empty = {};
+      Object.setPrototypeOf(empty, Rectangle.prototype);
+      return empty;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
