b28ced652e76077ad28ab3a0bc62a7b818f04040
Yebo Kalua <kaluayebo2@gmail.com>
2023-12-02 09:01:51 -0800

    correction adding redirection 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displays conditional body
-[ "$(curl -s -o /dev/null -w "%{http_code}" -L "$1")" -eq 200 ] && curl -s "$1"
+[ "$(curl -s -o /dev/null -w "%{http_code}" -L "$1")" -eq 200 ] && curl -s -L "$1"
