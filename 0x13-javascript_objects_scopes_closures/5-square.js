7813ca7904c98dd33714f832a02425568f5212db
Yebo Kalua <kaluayebo2@gmail.com>
2024-01-15 18:54:43 +0000

    square 2

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,41 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      const empty = {};
+      Object.setPrototypeOf(empty, Rectangle.prototype);
+      return empty;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
 class Square extends Rectangle {
-constructor (w, h, size) {
-    super(w, h);
+constructor (size) {
     this.size = size;
 }
 }
-module.exports = Square;
+module.exports = Rectangle;
