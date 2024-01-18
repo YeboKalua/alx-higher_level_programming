50b1c9c9f1b5a3933802ed91a6d5171901f067a7
Yebo Kalua <kaluayebo2@gmail.com>
2024-01-18 15:13:20 +0000

    square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,42 +1,9 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      const empty = {};
-      Object.setPrototypeOf(empty, Rectangle.prototype);
-      return empty;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'X';
-      }
-      console.log(row);
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
 constructor (size) {
     super(size, size);
-    this.size = size;
 }
 }
 module.exports = Rectangle;
