d0f4465cc05269c4711eb79f9717c093eee05f57
Yebo Kalua <kaluayebo2@gmail.com>
2024-01-28 01:13:15 -0800

    post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #posts to url
-curl -X -d POST "email=test@gmail.com&subject=I will always be here for PLD" -s "$1"
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" -s "$1"
